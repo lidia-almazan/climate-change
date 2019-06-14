88a3e415180c9ee00c5f3e4e3d72bec2fb27fdea
Lidia Almazan <lydiaalma@gmail.com>
2019-06-14 22:37:31 +0200

    Second submission with the first machine learning algorithm

--- climate-change-script.R
+++ climate-change-script.R
@@ -9,8 +9,8 @@
 ##################################
 
 # loading necessary library
-library(dplyr)
-library(tidyr)
+library(tidyverse)
+library(caret)
 library(ggplot2)
 
 # Data copied from https://www.kaggle.com/berkeleyearth/climate-change-earth-surface-temperature-data
@@ -40,7 +40,7 @@ qplot(Year,
       data=temp_spain_year_1800_2013,
       main="Spain Average Temperature 1800-2013",
       geom=c("point")) +
-  geom_smooth(method="loess") +
+  geom_smooth(method =  "loess",color="red",  span = 0.15, method.args = list(degree=1)) +
   aes(colour = Temp) +
   scale_color_gradient(low="blue", high="red") +
   ylab("Temperature (°C)")
@@ -51,15 +51,59 @@ temp_spain_month_1800_2013 <- temp_country_spain %>%
   filter(Year>1800) %>%
   group_by(Month)
 
-temp_spain_month_1800_2013$Month.String <- format(temp_spain_month_1800_2013$Month,"%B")
-
+# adding a new column with the month names instead of numbers
 temp_spain_month_1800_2013$Month.Name <- with(temp_spain_month_1800_2013, month.name[Month])
 
+# plot of the average temperature along the years for each month
 ggplot(temp_spain_month_1800_2013,
        aes(x=Year,y=AverageTemperature,colour=reorder(Month.Name, -AverageTemperature,mean)))+
   geom_point()+
-  geom_smooth(method="loess")+
+  geom_smooth(method =  "loess", span = 0.15, method.args = list(degree=1)) +
   labs(title="Average Temperatures by Month in Spain",
        x="Year",
-       y="Average Temperature",
+       y="Temperature (°C)",
        colour="Month")
+
+eurozone <- c("Austria","Belgium","Cyprus","Estonia","Finland","France","Germany",
+              "Greece","Ireland","Italy","Latvia","Lithuania","Luxembourg","Malta",
+              "Netherlands","Portugal","Slovakia","Slovenia","Spain")
+temp_eurozone_year_1800_2013 <- temp_country %>% 
+  filter(Country %in% eurozone) %>%
+  separate(col = dt, into = c("Year", "Month", "Day"), convert = TRUE) %>%
+  filter(Year>1800) %>%
+  group_by(Year,Country) %>% summarise(Temp = mean(AverageTemperature))
+
+# plot of the average temperature along the years for each country
+ggplot(temp_eurozone_year_1800_2013,
+       aes(x=Year,y=Temp,colour=reorder(Country, -Temp,mean)))+
+  geom_point()+
+  geom_smooth(method =  "loess", span = 0.15, method.args = list(degree=1)) +
+  labs(title="Average Temperatures by Month in Spain",
+       x="Year",
+       y="Temperature (°C)",
+       colour="Country")
+
+##########################################
+# Machine learning on the temperature data
+##########################################
+
+# creation of the data partition
+y <- temp_spain_year_1800_2013$Temp
+test_index <- createDataPartition(y, times = 1, p = 0.2, list = FALSE)
+
+train_set <- temp_spain_year_1800_2013 %>% slice(-test_index)
+test_set <- temp_spain_year_1800_2013 %>% slice(test_index)
+
+# average of the temperature ignoring the year influence
+m <- mean(train_set$Temp)
+m
+# computing the squared loss
+mean((m - test_set$Temp)^2)
+
+# using least square as method for estimating the slope taking into account the 
+# year
+fit <- lm(Temp ~ Year, data = train_set)
+fit$coef
+
+y_hat <- fit$coef[1] + fit$coef[2]*test_set$Year
+mean((y_hat - test_set$Temp)^2)
